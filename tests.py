571bdfba4e7b2071847484c97e35cf69a69c222b
loysopondohva <loysopondohva@yandex.com>
2025-05-28 07:15:16 +0300

    3 tests added

--- tests.py
+++ tests.py
@@ -37,3 +37,17 @@ class TestBooksCollector:
 #   def delete_book_from_favorites(self, name) - 1 позитив, 1 негатив (книги нет в избранном)
 #   def get_list_of_favorites_books(self) - 1 позитив
 
+    def test_empty_books_genre_and_favorites_true(self):
+        collector = BooksCollector()
+        assert collector.books_genre == {} and collector.favorites == []
+    
+    def test_genre_list_and_age_rating_list_true(self):
+        collector = BooksCollector()
+        expected_genre_list = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+        expected_genre_age_rating = ['Ужасы', 'Детективы']
+        assert collector.genre == expected_genre_list and collector.genre_age_rating == expected_genre_age_rating
+    
+    def test_add_new_book_14_letters_book_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Синий Трактор')
+        assert len(collector.get_books_genre()) == 1
